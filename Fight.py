7b58f2f623257764684853df87fcea7082569461
GStEugene <djakdragon51@gmail.com>
2021-08-26 18:42:54 +0300

    Новый фикс, старого фикса

--- Fight.py
+++ Fight.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 '''Это просто слегка расширенная версия подбрасывания монетки, не обращайте внимания.'''
 #---Imports---
-import colorama                             #! СТОП! если ты импортиш всю библиотеку целиком,
-from colorama import Fore, Back, Style      #! то не тащи из неё же функции по второму разу! from colorama import init вообщет религия не запрещает
+import colorama #Нужен для colorama.init(), иначе ругается, что coloram-ы нет в импортах
+from colorama import Fore, Back, Style #Без этой строчки выдаёт ошибку, мол Fore не задан
 from random import randrange
 from time import sleep
 from winsound import Beep
 
 #---Global---
-
+colorama.init() #Без этой строчки выводит текст стандартного форматирования("\033 [031", или как-то так) -
+                # - и при этом не меняет цвет текста, собственно почему встроенным методом не получилось воспользоваться
 #---Func---
 colorama.init()     #! ээ? выкинь ка нах в майн (лучше) или глобал это как бы всего то инициализатор раскраски а не твоя функция
 def RandomNumber(R_min=1, R_max=10+1):
     '''Функция RandomNumber
     Выдаёт случайное целое число в заданном промежутке или от 1 до 10.'''
     return randrange(R_min, R_max)
-    
+
+#------------- Ф-ции смены цвета текста -------------
+
+def Out_RED(text):
+    text = Fore.RED + text + Style.RESET_ALL
+    return text
+
+def Out_GREEN(text):
+    text = Fore.GREEN + text + Style.RESET_ALL
+    return text
+
+#----------------------------------------------------
+
 #---Class---
 class Character:
     hp = 100
@@ -29,8 +42,11 @@ class Character:
         print ('%s health points dropped to %d !' % (self.name, self.hp if self.hp > 0 else 0)) # второе условие это тренарный оператор
         sleep(0.5)
         
-    def attack(self):
-        #print ('\t%s attacked' % self.name)
+    def attack(self, color="Red"): #Тут print пофиксил как придумал, пока что-то по-другому сделать фантазии не хватило
+        Out = Out_RED('\t%s attacked' % self.name)
+        if color == "GREEN":
+            Out = Out_GREEN('\t%s attacked' % self.name)
+        print(Out)
         Beep(self.strikeSound, 25)
         return (RandomNumber() * self.strikePower)
         
@@ -48,34 +64,28 @@ distance = 5
 while retry == 'Y':
 
     Heros = [["Barbarian", 1, 5+1], ["Skeleton", 1, 5+1]] #Список персонажей
-    CC = int(input("Characters:\n1.Barbarian\n2.Skeleton\n3.Random\nChose your Hero:")) - 1 #(Chose_Character) Выбор персонажа  #! должен заметить ахуенность выбора имени, хоть не SS вермахта, но в коде хер поймёш что, и выбирать то 2 персов, через сплит можно разделить ввод на две переменные когда понадобиться
+    ChoseChar = int(input("Characters:\n1.Barbarian\n2.Skeleton\n3.Random\nChose your Hero:")) - 1 #(Chose_Character) Выбор персонажа
     print()
-    if CC == 2:
-        CC = RandomNumber(1, 3) - 1
-    Hero1 = Character(Heros[CC][0], RandomNumber(Heros[CC][1], Heros[CC][2]))
+    if ChoseChar == 2:
+        ChoseChar = RandomNumber(1, 3) - 1
+    Hero1 = Character(Heros[ChoseChar][0], RandomNumber(Heros[ChoseChar][1], Heros[ChoseChar][2]))
     Hero2 = Character('Skeleton', RandomNumber(1, 5+1))
     print('\nFight begins!\n')
 
-    print(Fore.RED + '\t%s attacked' % Hero2.name + Style.RESET_ALL)
-
     while True:
-
-        Hero2.hit(Hero1.attack())
-        print(Fore.GREEN + '\t%s attacked' % Hero1.name + Style.RESET_ALL)  #! Тупой вопрос но мож цвет лучше прописать в класс и передавать при создании(разные же будут? да?)?
+        Hero2.hit(Hero1.attack("GREEN"))
         if Hero2.hp < 1: break
         Hero1.hit(Hero2.attack())
-        print(Fore.RED + '\t%s attacked' % Hero2.name + Style.RESET_ALL)    #! Я вот тут что подумал, а заебень ка ты функцию которая сама будет возвращать или печатать сразу цветной текст (они как бы для этого и придуманы) а не занимася дрочивом клавиш, это знаеш ли не вознагрождаеться
         if Hero1.hp < 1: break
 
-
     if Hero1.hp > 0:
-        print(Fore.GREEN + '\n\t%s Wins!\n' % Hero1.name + Style.RESET_ALL)
+        print(Out_GREEN('\n\t%s Wins!\n' % Hero1.name))
     else:
-        print(Fore.RED + '\n\t%s Wins!\n' % Hero2.name + Style.RESET_ALL)
+        print(Out_RED('\n\t%s Wins!\n' % Hero2.name))
 
     del Hero1
     del Hero2
 
     retry = input('\nNew fight? (Y/N) ').upper()
     
-#---
+#----
