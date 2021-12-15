a7a665a418238c4999ba8d8a9ec51bbfe07072a1
GStEugene <djakdragon51@gmail.com>
2021-12-16 01:45:06 +0200

    удалена арена, добавлана возможность выбора расы и оружия

---
+++ config.py
@@ -0,0 +1,7 @@
+races = [['Human', {'strength':1, 'dexterity':4, 'defend':1}],
+         ['Turtle', {'strength':2, 'dexterity':1, 'defend':6}],
+         ['Minotaur', {'strength':5, 'dexterity':1, 'defend':1}]]
+
+weapon = [['fists', {'damage':5, 'attack_phrase':' ponch ', 'type_of_weapon':'melee'}],
+          ['battle staff', {'damage':6, 'attack_phrase':' hit ', 'type_of_weapon':'melee'}],
+          ['bow', {'damage':5, 'attack_phrase':' fired a bow at ', 'type_of_weapon':'ranged'}]]
